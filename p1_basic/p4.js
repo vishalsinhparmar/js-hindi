8d66b2d2675e9897df33a9a356d2f226c9ba3bce
vishalsinhparmar <143939203+vishalsinhparmar@users.noreply.github.com>
2023-09-20 10:21:03 +0000

    number convertion string

---
+++ p1_basic/p4.js
@@ -0,0 +1,31 @@
+let value = 3
+//console.log(-value);
+//console.log(2 + 2);
+//console.log(2 - 2);
+//console.log(2 * 2);
+//console.log(2 / 2);
+//console.log(2 % 2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+//console.log(num1,num2,num3);  
+
+let gameCounter = 100
+gameCounter++;
+//console.log(gameCounter);
+
+let x = 5
+let y = x++
+console.log(x,y);
+
+
