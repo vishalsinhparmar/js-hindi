ae9e60d625420f6cde5dfa95d400152042bcc98a
vishalsinhparmar <143939203+vishalsinhparmar@users.noreply.github.com>
2023-09-20 06:13:09 +0000

    conversion of datatype

---
+++ p1_basic/p3.js
@@ -0,0 +1,24 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInnumber = Number(score)
+//console.log(typeof valueInnumber);
+//console.log(valueInnumber);
+
+//"33"=>33
+//"33abc" => NaN
+//true => 1; false=>0
+
+let isLoggedIn = "vishal"
+let booleanLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"vishal"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
